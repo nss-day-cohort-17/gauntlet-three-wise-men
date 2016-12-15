d55969d4fea8707a315ffc68a72bd18a7c59b947
Wisbell <westleyisbell@gmail.com>
2016-12-14 20:53:40 -0600

    Added a working version of multiple rows for weapons column

--- javascripts/weapons.js
+++ javascripts/weapons.js
@@ -36,24 +36,55 @@ weaponsList.push(new Dagger())
 weaponsList.push(new BroadSword())
 weaponsList.push(new WarAxe())
 
+// below is just for testing
+weaponsList.push(new WarAxe())
+
+weaponsList.push(new WarAxe())
+weaponsList.push(new WarAxe())
+weaponsList.push(new WarAxe())
+weaponsList.push(new WarAxe())
+
+weaponsList.push(new WarAxe())
+weaponsList.push(new WarAxe())
+weaponsList.push(new WarAxe())
+weaponsList.push(new WarAxe())
+
 console.log(weaponsList)
 
 
 function addWeaponsDOM () {
+
+  var columnCount = -1;
+
   for(var i = 0; i < weaponsList.length; i++) {
 
     // Add a new column if the index equals 4
     if(i % 4 === 0) {
       $(".addWeaponsHere").append(`<div class="weaponColumn col-sm-4"></div>`)
+      //console.log($('.weaponColumn'))
+      console.log($('body').find('.weaponColumn'))
+      //console.log("how many columns weapons", $("body").find(".weaponColumn"))
+      columnCount++;
     }
+    // Add the new weapon to the DOM in the current column
+      //use .html() instead of .append()??
+        // $(".weaponColumn")[columnCount]
+        //$('body').find('.weaponColumn')[columnCount]
 
-    $(".weaponColumn").append(`  <div class="card__button">
-                                    <a class="weaponButton class__link btn btn--big btn--blue" href="#">
-                                      <span class="btn__prompt"></span>
-                                      <span class="btn__text">${weaponsList[i].name}</span>
-                                    </a>
-                                </div>`)
+       $('.weaponColumn')[columnCount].insertAdjacentHTML('beforeend',  `<div class="card__button">
+                                        <a class="weaponButton class__link btn btn--big btn--blue" href="#">
+                                        <span class="btn__prompt"></span>
+                                        <span class="btn__text">${weaponsList[i].name}</span>
+                                        </a>
+                                        </div>`)
+    // $('.weaponColumn')[columnCount].append(`<div class="card__button">
+    //                                         <a class="weaponButton class__link btn btn--big btn--blue" href="#">
+    //                                         <span class="btn__prompt"></span>
+    //                                         <span class="btn__text">${weaponsList[i].name}</span>
+    //                                         </a>
+    //                                         </div>`)
   }
+  //console.log("how many columns weapons", $("body").find(".weaponColumn"))
 }
 
 addWeaponsDOM()
